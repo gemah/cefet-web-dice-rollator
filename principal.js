6e76f026bd43a091cf8df44d1c041fb2cf3cdcd9
Hian <hianneiva@gmail.com>
2015-04-30 21:28:30 -0300

    Rolador de Dados

--- principal.js
+++ principal.js
@@ -11,3 +11,32 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+var botaoRolar = document.getElementById('rolar');
+
+botaoRolar.addEventListener('click', function () {
+
+    var dado = [4, 6, 8, 10, 12, 20];
+    var txtResult = '';
+    var soma = 0;
+
+    for (var i = 0; i < dado.length; i++) {
+        if (txtResult.length > 0)
+            txtResult += ' + ';
+        var nDado = dado[i];
+        var vDado = rolar(nDado);
+        txtResult += vDado;
+        soma += vDado;
+    }
+
+    var divResult = document.getElementById('resultado');
+    divResult.parentNode.classList.remove('oculto');
+    divResult.innerHTML = txtResult + ' = ' + soma;;
+
+    function rolar(n) {
+        var value = Math.ceil(Math.random() * n);
+        var dadoToDoc = document.getElementById('quantidadeD' + n);
+        dadoToDoc.value = value;
+        return value;
+    }
+
+});
